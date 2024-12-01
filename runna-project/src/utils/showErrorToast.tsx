2496950121ce1aed2560abf63162c02dab4ab308
tomasguell <tomasguell123@gmail.com>
2024-12-01 18:26:49 -0300

    mas tiempo de delay y se elimino la barra de progreso en los errores

--- runna-project/src/utils/showErrorToast.tsx
+++ runna-project/src/utils/showErrorToast.tsx
@@ -6,8 +6,8 @@ import ErrorToast from '../components/ui/ErrorToast';
 export const showErrorToast = (message: string, details: string): void => {
   toast.error(<ErrorToast message={message} details={details} />, {
     position: 'top-center',
-    autoClose: 5000,
-    hideProgressBar: false,
+    autoClose: 10000,
+    hideProgressBar: true,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
