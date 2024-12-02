7ef7b40b151335f62fbea2626f2b319235c074e9
tomasguell <tomasguell123@gmail.com>
2024-12-02 01:42:06 -0300

    new desing of details toast

--- runna-project/src/components/ui/ErrorToast.tsx
+++ runna-project/src/components/ui/ErrorToast.tsx
@@ -1,30 +1,54 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-
+import { Box, Typography, TextField, Button, Card, CardContent, CardHeader, IconButton } from '@mui/material';
+import { ContentCopy } from '@mui/icons-material'; // Icono para copiar texto
 interface ErrorToastProps {
   message: string;
   details: string;
 }
 
+const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text);
+  toast.info('¡Texto copiado al portapapeles!', {
+    position: 'top-center',
+    autoClose: 2000,
+    hideProgressBar: true,
+    theme: 'light',
+  });
+};
+
 const ErrorToast: React.FC<ErrorToastProps> = ({ message, details }) => {
-  const handleCopyDetails = () => {
-    navigator.clipboard.writeText(details);
-    toast.success('El codigo del error se copio al portapapeles.');
-  };
+ 
 
   return (
     <div>
       <p>{message}</p>
       <button
-        onClick={handleCopyDetails}
-        style={{
-          background: 'none',
-          border: 'none',
-          color: '#007BFF',
-          cursor: 'pointer',
-          textDecoration: 'underline',
-          padding: 0,
-          margin: 0,
+         onClick={() => {
+          // Expandir para mostrar detalles adicionales
+          toast.info(
+            <Box sx={{ whiteSpace: 'pre-wrap', bgcolor: '#f5f5f5', padding: '10px', borderRadius: '5px' }}>
+              <Typography variant="body2" fontFamily="monospace" sx={{ color: 'black', marginBottom: '5px' }}>
+                {details}
+              </Typography>
+              <IconButton
+                onClick={() => copyToClipboard(details)}
+                color="primary"
+                size="small"
+                aria-label="Copiar detalles"
+              >
+                <ContentCopy />
+              </IconButton>
+            </Box>,
+            {
+              position: 'top-center',
+              autoClose: false,
+              draggable: true,
+              closeOnClick: true,
+              hideProgressBar: true,
+              theme: 'colored',
+            }
+          );
         }}
       >
         Ver detalles
